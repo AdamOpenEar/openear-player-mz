dfc04ad9af449ccf6684b2ac5957dab09c6497e5
Adam Turtle <adam@cloudstation.co.uk>
2016-06-10 12:40:35 +0100

    dev api

--- src/tmpl/app.js
+++ src/tmpl/app.js
@@ -40,7 +40,7 @@ angular.module('OEPlayer',[
 }])
 /* @if NODE_ENV == 'DEVELOPMENT' */
 .constant('config',{
-    'api_url':'http://localhost/oe-apis/oe-player/v1/public/',
+    'api_url':'http://dev.openearmusic.com/player-api/',
     'module_dir':'ng',
     'local_path':'/',
     'file_extention':'.mp3',
