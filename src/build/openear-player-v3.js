d9c5884a0415d81b03808b612fe897a34b6e8951
Adam Turtle <adam@cloudstation.co.uk>
2016-12-07 18:04:15 +0000

    ‘delete stored data’ clears backlog

--- src/build/openear-player-v3.js
+++ src/build/openear-player-v3.js
@@ -44,7 +44,7 @@ angular.module('OEPlayer',[
     'local_path':'/',
     'file_extention':'.mp3',
     'log_path':'https://api.player.openearmusic.com/v1/log-track',
-    'version':'3.3.0.2'
+    'version':'3.3.0.3'
 })
 .controller('AppCtrl',['config','$scope',function(config,$scope){
     $scope.version = config.version;
@@ -344,6 +344,7 @@ angular.module('OEPlayer')
     $scope.deleteJson = function(){
         var c = confirm($scope.lang.settings.confstored);
         if(c){
+            localStorage.removeItem('backlog');
             FileFactory.readDirectory('')
                 .then(function(data){
                     for (var i = data.length - 1; i >= 0; i--) {
