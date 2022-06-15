f8b888adb31f0056e5695e0791bbec2160cbe9ab
Adam Turtle <adam@openearmusic.com>
2022-06-15 10:24:21 +0100

    update sw again

--- dev/ng/app.js
+++ dev/ng/app.js
@@ -41,7 +41,7 @@ angular.module('OEPlayer',[
     'local_path':'/',
     'file_extention':'.mp3',
     'log_path':'https://api.player.openearmusic.com/v2/log-track',
-    'version':'4.5.1 MULTI',
+    'version':'4.5.2 MULTI',
     'template':'ng/template/',
     'socket':'wss://ws.player.openearmusic.com'
 })
